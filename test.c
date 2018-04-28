24cdaffa8e2fbcf5dddc7cccc19540037650af93
dereksnyder42v2 <dereksnyder42@gmail.com>
2018-04-28 14:59:36 -0400

    added a plain text file for additional file transmission testing. Changes to test.c for file transmission also.

--- test.c
+++ test.c
@@ -1,31 +1,57 @@
 #include <stdio.h>
 #include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-#define PICTURE	""
+//#define FILENAME	"qty5.jpg.bz2"
+#define FILENAME	"haiku.txt"
 
-/* Prototypes */
+/* func.c Prototypes */
 int open_port(void);
 int write_port(int fd, const void *buf, size_t count);
 int close_port(int fd);
 
+/* TODO change main to write whatever file is passed from cmd line
+ * TODO add retry when # bytes written < 100
+ */
 int 
 main(void)
 {
 	int port_fd = open_port();
 	/* Do stuff */
 
-	/* file we want to send over port */
-	FILE *picture_fp = fopen(PICTURE, "r");
+	/* pointer to file we want to send over port */
+	FILE *sendfile_fp = fopen(FILENAME, "r");
 	struct stat st;
-	stat(PICTURE, &st);	/* stat() returns # bytes in file */
-	int filesize = st.st_size;
-	printf("filesize of picture is %d\n", filesize);
+	stat(FILENAME, &st);	/* stat() returns # bytes in file */
+	const int FILESIZE = st.st_size;
+	printf("Sending file of size %d B\n", FILESIZE);
+	/* descriptor of file we want to send */
+	int sendfile_fd = open(FILENAME, O_RDONLY);
 
+	/*
+	printf("Writing test string 'hello' to port\n");
 	int write_success = write_port(port_fd, "hello", 5);
 	printf("write_port() returned %d\n", write_success);
+	*/
+	
+	/* Now, to business.
+	 * Write file described by "sendfile_fd", 100 Bytes at a time.
+	 * On each pass, print # Bytes succesfully written/ returned from "read()" call
+	 */	
+	char* buf[100];
+	int num_bytes_read = 0;
+	int num_bytes_wrote;
+	for (int head = 0; head < FILESIZE; head += num_bytes_read)
+	{
+		num_bytes_read = read(sendfile_fd, &buf, 100); /* read up to 100 B */
+		num_bytes_wrote = write_port(port_fd, &buf, num_bytes_read);
+		printf("\tread %d B, wrote %d B\n", num_bytes_read, num_bytes_wrote);	
+	}
 
 	/* Done with stuff */
-	fclose(picture_fp);
+	fclose(sendfile_fp);
+	close(sendfile_fd);
 	int close_success = close_port(port_fd);
 	printf("close_port() returned %d\n", close_success);
 	return 0;
