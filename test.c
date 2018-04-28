7952443d5388b9833401401ebe2b1d7de9f3d750
dereksnyder42v2 <dereksnyder42@gmail.com>
2018-04-28 13:20:56 -0400

    working in some file writing capabilities

--- test.c
+++ test.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <sys/stat.h>
+
+#define PICTURE	""
 
 /* Prototypes */
 int open_port(void);
@@ -10,12 +13,20 @@ main(void)
 {
 	int port_fd = open_port();
 	/* Do stuff */
-	
+
+	/* file we want to send over port */
+	FILE *picture_fp = fopen(PICTURE, "r");
+	struct stat st;
+	stat(PICTURE, &st);	/* stat() returns # bytes in file */
+	int filesize = st.st_size;
+	printf("filesize of picture is %d\n", filesize);
+
 	int write_success = write_port(port_fd, "hello", 5);
 	printf("write_port() returned %d\n", write_success);
 
 	/* Done with stuff */
+	fclose(picture_fp);
 	int close_success = close_port(port_fd);
-
+	printf("close_port() returned %d\n", close_success);
 	return 0;
 }
