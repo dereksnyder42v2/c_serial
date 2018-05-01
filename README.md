1cb9dcc480200cfd47be7943d871f680d3dce4c6
dereksnyder42v2 <dereksnyder42@gmail.com>
2018-05-01 01:27:34 -0400

    implementing error correction, ACK/ NAK and sequence numbers, etc

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# c_serial
-Basic library for reading and writing over serial connections (UNIX)
+# diet-fi
+UNIX
+Basic library for reading and writing over serial connections (and RF, if you're into that). Includes some Forward Error Correction (FEC) functionality.
+
+- example application of sending Raspberry Pi camera photos included
+
---
+++ send_serial_3.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/* This is version 3--incorporates sequence numbers and checksums into packets for Forward Error Correction */
+
+/* func.c Prototypes */
+int open_port(void);
+int write_port(int fd, const void *buf, size_t count);
+int close_port(int fd);
+
+/* Other Prototypes */
+
+/* TCP style. Total length is  */
+struct Packet 
+{ 
+	short	src;		/* 2  source identifier */
+	short	dst;		/* 2  destination identifier */
+	int	seq;		/* 4  sequence number */
+	int	ack;		/* 4  acknowledgement number */
+	short	checksum;	// 2
+	char	len;		// 1
+	char	data[40];	// 40
+				/* 55 Byte packets */
+};
+
+/* TODO
+ * currently, this code attempts to open the /dev/ttyUSB0 device; funcs.c 
+ * must be manually changed to use a different device. it would be useful to
+ * specify the port &/or transmission Baud rate as command line args.
+ */
+int main(int argc, char* argv[])
+{
+	int port_fd = open_port();
+	/* Do stuff */
+
+	/* pointer to file we want to send over port */
+	FILE *sendfile_fp = fopen(argv[1], "r");
+	struct stat st;
+	stat(argv[1], &st);	/* stat() returns # bytes in file */
+	const int FILESIZE = st.st_size;
+	printf("Sending file of size %d B\n", FILESIZE);
+	
+	/* descriptor of file we want to send */
+	int sendfile_fd = open(argv[1], O_RDONLY);
+
+
+	/* Now, to business.
+	 * Write file described by "sendfile_fd", 1 Byte at a time.
+	 */
+	int bytes_read;
+	short ck;
+	int bytes_written_temp;
+	int file_offset = 0; 	/* 'how far in' to the file we are sending */
+	
+	struct Packet pac;
+	/* Not using these fields for now...*/
+	pac.src = 	(short)0;
+	pac.dst = 	(short)0;
+	pac.seq = 	(int)0;
+	pac.ack = 	(int)0;
+	/* ... */
+	pac.checksum = 	(short)0;
+	pac.len = 	(char)0;
+	while ( file_offset < FILESIZE )
+	{
+		bytes_read = pread(sendfile_fd, &pac.data, 40, file_offset);
+		ck = 0;
+		for (int i = 0; i < bytes_read; i++)
+			ck += (short)*(&pac.data + i);
+		pac.checksum = ck;
+		pac.len = bytes_read;
+		/* the while block below continues to send the same Byte until "write_port()"
+		 * returns 1; -1 indicates an error (e.g. resource busy, no data written) 
+		 * while 0 indicates no Bytes were written.
+		 * TODO figure out why so many "resource busy" errors
+		 */
+		for (int i = 0; i < 55; i++)
+		{
+			while ( bytes_written_temp != 1 )
+			{
+				// TODO change &buf to appropriate variable
+				bytes_written_temp = write_port(port_fd, (&pac + i), 1);
+			}
+			bytes_written_temp = 0;
+		}
+		
+		file_offset += bytes_read;
+	}
+	
+	/* Done with stuff */
+	fclose(sendfile_fp);
+	close(sendfile_fd);
+	int close_success = close_port(port_fd);
+	printf("close_port() returned %d\n", close_success);
+
+	return 0;
+}
+
